2b3b3175beb9318adc2737e0989612469dc81d07
Gaurav <gauravenigma@gmail.com>
2024-03-05 22:43:25 +0000

    Redirectors &>

--- backend.sh
+++ backend.sh
@@ -1,36 +1,36 @@
 
 #Install NodeJS
-dnf module disable nodejs -y >/tmp/expense.log
-dnf module enable nodejs:20 -y >/tmp/expense.log
-dnf install nodejs -y >/tmp/expense.log
+dnf module disable nodejs -y &>/tmp/expense.log
+dnf module enable nodejs:20 -y &>/tmp/expense.log
+dnf install nodejs -y &>/tmp/expense.log
 
 #Add application User...
-useradd expense >/tmp/expense.log
-cp backend.service /etc/systemd/system/backend.service >/tmp/expense.log
+useradd expense &>/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service &>/tmp/expense.log
 
 #Lets setup an app directory. Remove any old one
-rm -rf /app >/tmp/expense.log
-mkdir /app >/tmp/expense.log
+rm -rf /app &>/tmp/expense.log
+mkdir /app &>/tmp/expense.log
 
 #Download the application code to created app directory.
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip >/tmp/expense.log
-cd /app >/tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>/tmp/expense.log
+cd /app &>/tmp/expense.log
 
 
-unzip /tmp/backend.zip >/tmp/expense.log
+unzip /tmp/backend.zip &>/tmp/expense.log
 
-cd /app >/tmp/expense.log
-npm install >/tmp/expense.log
+cd /app &>/tmp/expense.log
+npm install &>/tmp/expense.log
 
 
-systemctl daemon-reload >/tmp/expense.log
+systemctl daemon-reload &>/tmp/expense.log
 
-systemctl enable backend >/tmp/expense.log
-systemctl start backend >/tmp/expense.log
+systemctl enable backend &>/tmp/expense.log
+systemctl start backend &>/tmp/expense.log
 
-dnf install mysql -y >/tmp/expense.log
+dnf install mysql -y &>/tmp/expense.log
 
-mysql -h 172.31.24.177 -uroot -pExpenseApp@1 < /app/schema/backend.sql >/tmp/expense.log
+mysql -h 172.31.24.177 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>/tmp/expense.log
 
 # restart
 #sudo systemctl restart backend
