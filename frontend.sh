bca53c9198a003b400be00fa5bcaa6f9451aaa8d
Gaurav <gauravenigma@gmail.com>
2024-03-10 22:12:18 +0000

    Redirectors &>

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,16 @@
 
-#installign Nginx
+#*****************   Installing Nginx    *********************
+echo "Installing Nginx"
 dnf install nginx -y
 
-#Enable and start seervice
+
+#**********     Enable and start service       ***************
+echo "Enable and start service "
 systemctl enable nginx
 systemctl start nginx
 
-#Copy expense config file to the right place....
+#******Copy expense config file to the right place....
+echo "Copy expense config file to the right place...."
 cp expense.conf /etc/nginx/default.d/expense.conf
 
  #Remove the old content
